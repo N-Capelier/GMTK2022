43a111802b8ebb41efaa519d671868bd0ac7e492
Nicolas Capelier <capelier.nicolas@gmail.com>
2022-07-17 00:31:38 +0200

    Added enemy spawner

--- GMTK2022-FPS-Proto/Assets/Scripts/Enemies/EnemySpawner.cs
+++ GMTK2022-FPS-Proto/Assets/Scripts/Enemies/EnemySpawner.cs
@@ -1,10 +1,54 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
 	[SerializeField] GameObject enemyPrefab;
 
+	[SerializeField] float baseTimeBetweenSpawn;
+	[SerializeField] float maxTimeBetweenSpawn;
 
+	float timeBetweenSpawn;
+	float nineMinutes;
+
+	Clock spawnTimer;
+
+	private void Start()
+	{
+		nineMinutes = 60f * 9f;
+		timeBetweenSpawn = baseTimeBetweenSpawn;
+
+		spawnTimer = new Clock();
+		spawnTimer.ClockEnded += SpawnTimer_ClockEnded;
+		spawnTimer.SetTime(timeBetweenSpawn);
+
+		LerpTimeBetweenSpawn();
+	}
+
+	private void SpawnTimer_ClockEnded()
+	{
+		SpawnEnemy();
+		spawnTimer.SetTime(timeBetweenSpawn);
+	}
+
+	void SpawnEnemy()
+	{
+		Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+	}
+
+	IEnumerator LerpTimeBetweenSpawn()
+	{
+		float elapsedTime = 0f;
+		float completion;
+
+		while(elapsedTime < nineMinutes)
+		{
+			elapsedTime += Time.deltaTime;
+			completion = elapsedTime / nineMinutes;
+			timeBetweenSpawn = Mathf.Lerp(baseTimeBetweenSpawn, maxTimeBetweenSpawn, completion);
+			yield return new WaitForEndOfFrame();
+		}
+
+		yield return new WaitForEndOfFrame();
+	}
 }
